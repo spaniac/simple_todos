52da919522727cb309102badf87ba54f93c7b27e
박세준 <spaniac2@gmail.com>
2020-02-10 15:07:57 +0900

    이미지 업로드 시 확장자를 붙이도록 수정

--- todo_app/views.py
+++ todo_app/views.py
@@ -23,18 +23,17 @@ class TodoRUDView(RetrieveUpdateDestroyAPIView):
         instance = self.get_object()
         if request.data.get('completed'):
             instance.completed_at = datetime.now()
-        serializer = self.get_serializer(instance, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-
         if request.FILES:
             legacy_file = instance.image_url
             if legacy_file:
                 image_delete(legacy_file)
-            _, file = request.FILES.items()[0]
+            _, file = request.FILES.items().__next__()
             uploaded_filename = image_upload(file)
             instance.image_url = uploaded_filename
 
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
         return Response(serializer.data)
 
     def destroy(self, request, *args, **kwargs):
@@ -55,20 +54,21 @@ class TodoListCreateView(ListCreateAPIView):
         self.perform_create(serializer)
 
         if request.FILES:
-            _, file = request.FILES.items()[0]
+            _, file = request.FILES.items().__next__()
             uploaded_filename = image_upload(file)
             created_todo = self.queryset.filter(id=serializer.data.get('id')).first()
             created_todo.image_url = uploaded_filename
             created_todo.save()
 
+        result_todo = Todo.objects.filter(id=serializer.data.get('id')).first()
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(self.get_serializer(result_todo).data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 def image_upload(file):
-    random_filename = uuid4().hex
+    extension = file.name.split('.')[-1]
+    random_filename = uuid4().hex + '.' + extension
     path = IMAGE_DIR + random_filename
-    print('file: {}'.format(file))
     with open(path, '+wb') as dest:
         for chunk in file.chunks():
             dest.write(chunk)
